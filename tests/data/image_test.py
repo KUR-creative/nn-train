0677a8fff6e418f1fa5a9fa3a8efaac85d83d32d
kur <rhdnfka94@gmail.com>
2019-12-12 16:20:22 +0900

    Add rbk1hot fixture and test with it

--- tests/data/image_test.py
+++ tests/data/image_test.py
@@ -10,9 +10,9 @@ from nnlab.data import image as im
 from nnlab.utils import image_utils as iu
 
 def test_map_rbk_mask_to_1hot_mask():
-    rgb_1hot = bidict({(255,  0,  0): (0.,0.,1.),
-                       (  0,  0,255): (0.,1.,0.),
-                       (  0,  0,  0): (1.,0.,0.)})
+    rgb_1hot = bidict({(255,  0,  0): (  0,  0,255),
+                       (  0,  0,255): (  0,255,  0),
+                       (  0,  0,  0): (255,  0,  0)})
 
     img = cv2.imread('./tests/fixtures/masks/rbk.png')
     mapped = im.map_colors(img, rgb_1hot.inverse)
@@ -25,6 +25,8 @@ def test_map_rbk_mask_to_1hot_mask():
 
     # NOTE: Look and Feel test!
     #cv2.imshow('mask', mapped.astype(np.float64)); cv2.waitKey(0)
+    expected = cv2.imread('./tests/fixtures/masks/rbk1hot.png')
+    assert np.array_equal(mapped, expected)
 
 @pytest.mark.xfail(raises=deal._exceptions.PreContractError)
 def test_map_mask_colors_with_insufficient_1hot_dic():
