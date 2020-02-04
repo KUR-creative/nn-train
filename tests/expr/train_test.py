138a9e5d40aab2d2d9592817567e8011a0400139
kur <rhdnfka94@gmail.com>
2020-02-04 14:05:34 +0900

    Fix: Test of random crop using bigger input

--- tests/expr/train_test.py
+++ tests/expr/train_test.py
@@ -2,8 +2,8 @@ from nnlab.expr import train
 import tensorflow as tf
 
 def test_crop_generate_random_crops():
-    img = tf.reshape(tf.linspace(0., 26., 25), (5,5,1))
-    mask = tf.reshape(tf.linspace(0., -26., 25), (5,5,1))
+    img = tf.reshape(tf.linspace(0., 26., 250000), (500,500,1))
+    mask = tf.reshape(tf.linspace(0., -26., 250000), (500,500,1))
     print(img, mask)
     im, ma = train.crop(img, mask, 3)
     print(im.numpy().reshape((3,3)))
